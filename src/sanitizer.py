8c146ab6004ce97bb096b230b0d1f43daae716df
chris-krenz <ckrenz@bu.edu>
2024-04-28 23:48:45 -0400

    Tweaked input sanitization.

--- src/sanitizer.py
+++ src/sanitizer.py
@@ -3,14 +3,14 @@ Contains function to check for potentially malicious code.
 """
 
 
-def sanitizer(message: str) -> str:
+def sanitizer(message: bytes | str) -> str:
     """
     Sanitize message to ensure not malicious.  Primarily filters out SQL Queries.
     Raises exception if match is found; otherwise returns string indicating message is clean.
     The coincidental combination of these words with a ';' is relatively unlikely.
     Will be false positives, but erring on the side of safety.
     """
-    message = message.decode()
+    message = message.decode() if type(message) == bytes else message
 
     # (must also contain a ';')
     restricted_sql = [['select',   'from'],
@@ -26,8 +26,8 @@ def sanitizer(message: str) -> str:
                       ['rollback', 'to']]
 
     if ';' in message:
-        for set in restricted_sql:
-            if all([(keyword in message.lower()) for keyword in set]):
+        for keyword_set in restricted_sql:
+            if all([(keyword in message.lower()) for keyword in keyword_set]):
                 print('TESTING')
                 raise RuntimeError('You entered illegal characters, please try again...')
 
